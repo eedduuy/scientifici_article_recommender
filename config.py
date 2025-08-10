92bb79674991dffcba8b369270c2fa4e87f91c5b
eedduuy <eedduuy@gmail.com>
2025-08-10 19:11:26 +0200

    Added embeddings exploring

--- config.py
+++ config.py
@@ -3,11 +3,14 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / 'data'
+MODELS_DIR = BASE_DIR / 'models'
+SRC_DIR = BASE_DIR / 'src'
+
+
 DATA_RAW_DIR = DATA_DIR / 'raw'
 DATA_PROCESSED_DIR = DATA_DIR / 'processed'
 DATA_INTER_DIR = DATA_DIR / 'intermediate'
-MODELS_DIR = BASE_DIR / 'models'
-SRC_DIR = BASE_DIR / 'src'
+
 
 # Paths to data files
 ARXIV_DATASET = DATA_RAW_DIR / 'arxiv.json'
